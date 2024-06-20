4ad5ed34c9fe59020276b9c7d6a65c0e435f3aaa
Ismail <sinovkanal0@gmail.com>
2024-06-20 11:34:06 +0500

    users app ayaratildi. Register va Login uchun template qo'shildi

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import landing_page
 urlpatterns = [
     path('', landing_page, name='landing_page'),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- goodreads/views.py
+++ goodreads/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def landing_page(request):
-    return HttpResponse("<h1>Landing Page</h1>")
+    return render(request, 'landing.html')
