21ab1ffb2bc1fe3b876731d583df689c9a2d8925
Ismail <sinovkanal0@gmail.com>
2024-06-20 14:31:10 +0500

    User registration uchun form va view yozildi

--- users/views.py
+++ users/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.views import View
-
+from users.forms import UserCreateForm
 
 # Create your views here.
 
 class UserRegistrationView(View):
     def get(self, request):
-        return render(request, 'users/register.html')
+        create_form = UserCreateForm()
+        context = {
+            'form': create_form
+        }
+        return render(request, 'users/register.html', context)
+
+    def post(self, request):
+        create_form = UserCreateForm(request.POST)
+        if create_form.is_valid():
+            create_form.save()
+            return redirect('users:login')
+        else:
+            context = {
+                'form': create_form
+            }
+            return render(request, 'users/register.html', context)
+
+class UserLoginView(View):
+    def get(self, request):
+        return render(request, 'users/login.html')
